52760f38ac4383c7944376fa3ed5fa797c68c889
howdy39 <tatsuya.howdy@gmail.com>
2019-01-27 14:56:50 +0900

    fix: add 003

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 /* eslint-env browser */
 import './assets/css/style.css';
 
-const ul = document.createElement('ul');
-addLink(ul, {num: '002', text: 'JavaScriptの書き方を知りたい'});
-
 const app = document.getElementById('app');
+const ul = getUl();
 
 if (app) {
   app.appendChild(ul);
+}
+
+function getUl() {
+  const ul = document.createElement('ul');
+  addLink(ul, {num: '002', text: 'JavaScriptの書き方を知りたい'});
+  addLink(ul, {num: '003', text: 'JavaScriptを別ファイルに書きたい'});
+  return ul;
 }
 
 function addLink(ul, {num, text}) {
